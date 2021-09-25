6b7968d824f0f7202b7a54dd72922ef7bc815a80
thomazdev2020 <thomaz.dev.2020@gmail.com>
2021-09-25 20:12:09 -0300

    primeiro Commit

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>WDEV Vagas</title>
+</head>
+
+<body class="bg-dark text-light">
+    <div class="container">
+        <div class="jumbotron bg-danger">
+            <h1>Wdev Vagas</h1>
+            <p>exemplo de GRUD com PHP</p>
+        </div>
---
+++ includes/listagem.php
@@ -0,0 +1,8 @@
+<main>
+    <section>
+        <a href="cadastrar.php">
+            <button class="btn btn-sucess">Cadastrar</button>
+        </a>
+    </section>
+
+</main>
